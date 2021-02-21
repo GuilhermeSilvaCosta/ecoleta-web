ebcd4b18b8b850dcf1fc15f3921915c92875898e
Guilherme <tec.guilherme.dsc@gmail.com>
2021-02-21 19:54:02 -0300

    Updates

---
+++ precache-manifest.7353b3b5e4a8b6009de035a53b5c5f4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18bab11e411367e7ec9312519df66a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6bc9ce676c94a30369",
+    "url": "/static/css/main.6f652249.chunk.css"
+  },
+  {
+    "revision": "c292924c2b780f3dddd0",
+    "url": "/static/js/2.ed362eed.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/static/js/2.ed362eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6bc9ce676c94a30369",
+    "url": "/static/js/main.a69f6f5e.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
